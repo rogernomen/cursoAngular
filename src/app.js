461e52675511a6a54c6f123c703b9b4431be259a
Roger Nomen <ro.nomen@ext.desigual.com>
2016-02-05 22:19:06 +0100

    step2

---
+++ src/app.js
@@ -0,0 +1,34 @@
+var app = angular.module("app", []);
+
+app.controller("firstController", function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
+
+angular.module("app")
+    .controller("secondController", function () {
+        var self = this;
+
+        self.firstName = "John";
+        self.lastName = "Doe";
+    })
+    .controller("thirdController", function () {
+        var self = this;
+
+        self.print = "This is working too";
+    });
+
+
+//// BEST OPTION
+//app.controller("secondController", ['$scope', function($scope) {
+//    $scope.firstName = "John";
+//    $scope.lastName = "Doe";
+//}]);
+//
+//
+//// BETTER OPTION
+//app.controller("secondController", ['$scope', secondController($scope)]);
+//
+//function secondController($scope) {
+//
+//}
